0bf6234dcc1709e0603f3c5b5710ac81650117d4
moranjani <morancha@gmail.com>
2019-11-19 17:55:29 +0200

    solution for different letters

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -3,5 +3,17 @@ package mastermind
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    TODO()
+    var correctPosition = 0
+    var incorrectPosition = 0
+    for (i in guess.indices) {
+        if (guess[i] == secret[i]) {
+            correctPosition++;
+        } else {
+            if (secret.contains(guess[i])) {
+                incorrectPosition++
+            }
+        }
+    }
+    var eval = Evaluation(correctPosition, incorrectPosition)
+    return eval
 }
