26d785c66fca061eff8b126608550f0b55ec3057
moranjani <morancha@gmail.com>
2019-11-19 19:26:49 +0200

    full solution

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -5,15 +5,38 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 fun evaluateGuess(secret: String, guess: String): Evaluation {
     var correctPosition = 0
     var incorrectPosition = 0
+    var secretCopy = secret
+    var guessCopy = guess
+
+    val quickRightPos = secret.zip(guess).count {it.first == it.second}
+
+
     for (i in guess.indices) {
         if (guess[i] == secret[i]) {
-            correctPosition++;
-        } else {
-            if (secret.contains(guess[i])) {
-                incorrectPosition++
-            }
+            correctPosition++
+            secretCopy = secretCopy.replaceFirst(guess[i].toString(), "")
+            guessCopy = guessCopy.replaceFirst(guess[i].toString(), "")
+        }
+    }
+    for (i in guessCopy.indices) {
+        if (secretCopy.contains(guessCopy[i])) {
+            incorrectPosition++
+            secretCopy = secretCopy.replaceFirst(guess[i].toString(), "")
+
         }
     }
     var eval = Evaluation(correctPosition, incorrectPosition)
     return eval
+}
+
+
+fun evaluateGuessFunctionalSolution(secret: String, guess: String): Evaluation {
+
+    val rightPositions = secret.zip(guess).count { it.first == it.second }
+
+    val commonLetters = "ABCDEF".sumBy { ch ->
+
+        Math.min(secret.count { it==ch }, guess.count { it==ch })
+    }
+    return Evaluation(rightPositions, commonLetters - rightPositions)
 }
